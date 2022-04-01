14ef41f0b346058f43fc9d2c228bef36eb37dc69
Kristen Bartel <kristenbartel@gmail.com>
2022-03-31 20:45:28 -0500

    part 1 complete

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './MemoryCard.css';
+
+class MemoryCard extends React.Component {
+    render() {
+        return <div className='MemoryCard'>
+        <img src='flipflop.png' alt='flip-flip-logo'></img>
+        </div>
+    }
+}
+
+export default MemoryCard;
