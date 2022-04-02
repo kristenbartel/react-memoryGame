4e872eb9f27efa56d6e11bfb3c330da6c8452c19
Kristen Bartel <kristenbartel@gmail.com>
2022-04-02 13:01:49 -0500

    step3 complete

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,21 +2,14 @@ import React, { Component } from 'react'
 import './MemoryCard.css';
 
 class MemoryCard extends React.Component {
-    constructor() {
-        super();
-        this.state = { isFlipped: false };
-    }
-    clickHandler() {
-        this.setState({ isFlipped: !this.state.isFlipped});
-    }
     render() {
         return  <> 
-                <div className='MemoryCard' onClick={this.clickHandler.bind(this)}> 
-                <div className={!this.state.isFlipped ? 'MemoryCardInner' : 'MemoryCardInner flipped'}>
+                <div className='MemoryCard' onClick={this.props.pickCard}> 
+                <div className={!this.props.isFlipped ? 'MemoryCardInner' : 'MemoryCardInner flipped'}>
                     <div className='MemoryCardBack'> 
                         <img src='flipflop.png' alt='flip-flip-logo'></img>
                     </div>
-                    <div className='MemoryCardFront'>∆</div>
+                    <div className='MemoryCardFront'>{this.props.symbol}</div>
                 </div>
                 </div>
                 </>
